42404fd9502d7177977ef0351f309199b9306aaf
Peter Toth <tothpeter08@gmail.com>
2017-02-07 20:33:13 +0100

    Default params

--- pluralsight_getting_started_with_elixir/3_modules_and_fuctions/04-functions.exs
+++ pluralsight_getting_started_with_elixir/3_modules_and_fuctions/04-functions.exs
@@ -5,8 +5,19 @@ defmodule Sample.Enum do
   def first2(list) when length(list) == 0, do: nil
   def first2([head | _]), do: head
 
+
+  def first3(list, val \\ nil)
+
+  def first3([], val), do: val
+  def first3([head | _], _), do: head
+
   def last([head | []]), do: head
   def last([_ | tail]), do: last(tail)
+
+
+  def add(list, val \\ 0) do
+    [val | list]
+  end
 end
 
 defmodule Sample.Calendar do
@@ -24,5 +35,9 @@ end
 # IO.inspect Sample.Enum.last([1,2,3])
 
 # IO.inspect Sample.Enum.first2([1,2,3])
-IO.inspect Sample.Calendar.is_leep_year?(2016)
-IO.inspect Sample.Calendar.is_leep_year?
+# IO.inspect Sample.Calendar.is_leep_year?(2016)
+# IO.inspect Sample.Calendar.is_leep_year?
+
+# IO.inspect Sample.Enum.add([1,2])
+IO.inspect Sample.Enum.first3([], :defaul)
+IO.inspect Sample.Enum.first3([1], :defaul)
