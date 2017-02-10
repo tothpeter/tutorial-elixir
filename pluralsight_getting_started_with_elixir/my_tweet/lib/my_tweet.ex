bd4fa8a5102f01ae37ff2c29dd831962e0778ab2
Peter Toth <tothpeter08@gmail.com>
2017-02-10 18:59:13 +0100

    Add empty application

---
+++ pluralsight_getting_started_with_elixir/my_tweet/lib/my_tweet.ex
@@ -0,0 +1,18 @@
+defmodule MyTweet do
+  @moduledoc """
+  Documentation for MyTweet.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MyTweet.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ pluralsight_getting_started_with_elixir/my_tweet/test/my_tweet_test.exs
@@ -0,0 +1,8 @@
+defmodule MyTweetTest do
+  use ExUnit.Case
+  doctest MyTweet
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
