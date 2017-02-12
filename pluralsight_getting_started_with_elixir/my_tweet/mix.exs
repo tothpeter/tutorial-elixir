89762e884709f43f2573599fee2343ee5d2f2ab9
Peter Toth <tothpeter08@gmail.com>
2017-02-12 20:09:13 +0100

    Add quantum package

--- pluralsight_getting_started_with_elixir/my_tweet/mix.exs
+++ pluralsight_getting_started_with_elixir/my_tweet/mix.exs
@@ -15,7 +15,7 @@ defmodule MyTweet.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger],
+    [extra_applications: [:logger, :quantum],
      mod: {MyTweet.Application, []}]
   end
 
@@ -29,6 +29,8 @@ defmodule MyTweet.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      { :quantum, "~> 1.9" }
+    ]
   end
 end
